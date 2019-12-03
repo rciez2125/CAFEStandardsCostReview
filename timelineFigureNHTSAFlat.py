b65ff76c755133d32b8f8e09b155ab4286119e2d
rciez2125 <rebecca.ciez@gmail.com>
2019-12-03 15:06:00 -0500

    update timeline figure options

--- timelineFigureNHTSAFlat.py
+++ timelineFigureNHTSAFlat.py
@@ -46,13 +46,14 @@ def makeAFigure():
 	y_epa = 0
 	y_ca = 5
 
-	y_merged = y_epa 
-	 
+	
 	x_merge = 2009
 
 	fs = 6
-	lineH = 1
+	lineH = 0.5
 	tickH = 1
+
+	y_merged = y_nhtsa + lineH#y_epa 
 
 	data = pd.read_csv("NHTSACAFEData14Aug2019.csv")
 	car = data[data.Year > 1977]
@@ -87,9 +88,9 @@ def makeAFigure():
 	truckTrump['Year'] = yr
 
 	prenhtsa_line = plt.Rectangle((1972, y_nhtsa-lineH/2),3, lineH, color = nhtsa, linewidth = 0, alpha = 0.4)
-	nhtsa_line = plt.Rectangle((1975, y_nhtsa-lineH/2), (x_merge-1975-0.5), lineH, color = nhtsa, linewidth = 0, alpha = 1)
+	nhtsa_line = plt.Rectangle((1975, y_nhtsa-lineH/2), (x_merge-1975), lineH, color = nhtsa, linewidth = 0, alpha = 1)
 	nhtsa_connector = plt.Rectangle((x_merge-lineH/2, y_nhtsa+lineH/2), lineH, y_merged-y_nhtsa-2*lineH, color = nhtsa, linewidth=0)
-	mergednhtsa_line = plt.Rectangle((x_merge+0.5, y_merged-3*lineH/2), (2026.5-x_merge-0.5), lineH, color = nhtsa, linewidth = 0, alpha = 1)
+	mergednhtsa_line = plt.Rectangle((x_merge, y_merged-3*lineH/2), (2026.5-x_merge), lineH, color = nhtsa, linewidth = 0, alpha = 1)
 	plt.text(1971.5, y_nhtsa, 'NHTSA', color = nhtsa, fontsize = fs+2, horizontalalignment = 'right', verticalalignment = 'center')
 
 	nhtsaW1 = mpatches.Wedge((x_merge-0.5, y_nhtsa+lineH/2), lineH, 270, 360, color = nhtsa, linewidth = 0)
@@ -116,7 +117,7 @@ def makeAFigure():
 
 		plt.plot(1996, y_nhtsa, '.k') #light trucks only 
 		plt.plot((1996, 1996), (y_nhtsa, y_nhtsa-2*lineH), '-k')
-		plt.text(1997, y_nhtsa-2.25*lineH, '1996: New LT Standard,\n20.7 mpg', fontsize = fs, horizontalalignment = 'right', verticalalignment = 'top')
+		plt.text(1998, y_nhtsa-2.25*lineH, '1996: New LT Standard,\n20.7 mpg', fontsize = fs, horizontalalignment = 'right', verticalalignment = 'top')
 
 		plt.plot(2003, y_nhtsa, '.k')
 		plt.plot((2003, 2003), (y_nhtsa, y_nhtsa-4*tickH), '-k')
@@ -127,9 +128,11 @@ def makeAFigure():
 		plt.text(2007-0.5, y_nhtsa-6.25*tickH, '2007: EISA Passed, Set passenger car standard of 35.0\nmpg by 2020, required new NHTSA standard for\nlight trucks', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'top')#, rotation = 90)
 	addNHTSADates()
 
-	prepa_line = plt.Rectangle((2005, y_epa-lineH/2), 2009-2005, lineH, color = epa, linewidth = 0, alpha = 0.5)
-	#epa_line = plt.Rectangle((2009, y_epa-lineH/2), 2, lineH, color = epa, linewidth = 0, alpha = 1)
-	mergedepa_line = plt.Rectangle((x_merge, y_merged-lineH/2), (2026.5-x_merge), lineH, color = epa, linewidth = 0, alpha = 1)
+	prepa_line = plt.Rectangle((2005, y_epa-lineH/2), 2009-2005-lineH, lineH, color = epa, linewidth = 0, alpha = 0.5)
+	mergedepa_line = plt.Rectangle((x_merge+lineH/2, y_merged-lineH/2), (2026.5-x_merge-lineH/2), lineH, color = epa, linewidth = 0, alpha = 1)
+	epaW1 = mpatches.Wedge((x_merge-lineH, y_epa-lineH/2), lineH, 0, 90, color = epa, linewidth = 0)
+	epaW2 = mpatches.Wedge((x_merge+lineH, y_merged+3*lineH/2), lineH*2, 180, 270, color = epa, linewidth = 0)
+	epa_connector = plt.Rectangle((x_merge-lineH, y_merged+lineH), lineH, y_epa-y_merged-lineH, color = epa, linewidth=0)
 
 	def addEPADates():
 		plt.plot(2009, y_epa, '.k')
@@ -144,27 +147,27 @@ def makeAFigure():
 	preca_line = plt.Rectangle((2007, y_ca-lineH/2), 2009-2007, lineH, color = ca, linewidth = 0, alpha = 0.5)
 	#ca_line = plt.Rectangle((2005, y_ca-lineH/2), x_merge-2005-0.5, lineH, color = ca, linewidth = 0, alpha = 1)
 	mergedca_line = plt.Rectangle((x_merge+0.5, y_merged+lineH/2), 2026.5-x_merge-0.5, lineH, color = ca, linewidth = 0, alpha = 1)
-	caW1 = mpatches.Wedge((x_merge-lineH/2, y_ca-lineH/2), lineH, 0, 90, color = ca, linewidth = 0)
-	caW2 = mpatches.Wedge((x_merge+lineH/2, y_merged + 3*lineH/2), lineH, 180, 270, color = ca, linewidth = 0)
-	ca_connector = plt.Rectangle((x_merge-lineH/2, y_merged+3*lineH/2), lineH, y_ca-y_merged-2*lineH, color = ca, linewidth=0)
+	caW1 = mpatches.Wedge((x_merge, y_ca-lineH/2), lineH, 0, 90, color = ca, linewidth = 0)
+	caW2 = mpatches.Wedge((x_merge+lineH, y_merged + 3*lineH/2), lineH, 180, 270, color = ca, linewidth = 0)
+	ca_connector = plt.Rectangle((x_merge, y_merged+3*lineH/2), lineH, y_ca-y_merged-2*lineH, color = ca, linewidth=0)
 	plt.text(2006.5, y_ca, 'CARB', horizontalalignment='right', verticalalignment = 'center', color = ca, fontsize = fs+2)
 	#plt.text(1999.5, y_ca+3*lineH/2, 'Anything in CA before they got their waiver in 2009 we should mention?', fontsize=fs, horizontalalignment='right')
 
 	def addMergedDates():
 		plt.plot(2012, y_merged, '.k')
-		plt.plot((2012, 2012), (y_merged, y_merged-5*tickH), '-k')
+		plt.plot((2012, 2012), (y_merged, y_merged-3*tickH), '-k')
 		#plt.text(2011.5, y_merged-5.25*tickH, '2012 Obama Admin. Sets new standards\nfor 2017-2025, 54.5 mpg overall by 2025', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'top')
-		plt.text(2011.5, y_merged-5.25*tickH, '2012: New standards for 2017-2025,\n54.5 mpg overall by 2025', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'top')	
+		plt.text(2011.5, y_merged-3.25*tickH, '2012: New standards for 2017-2025,\n54.5 mpg overall by 2025', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'top')	
 
 		plt.plot(2018.67, y_merged, '.k')
-		plt.plot((2018.67, 2018.67), (y_merged, y_merged+9*lineH), '-k')
+		plt.plot((2018.67, 2018.67), (y_merged, y_merged+18*lineH), '-k')
 		#plt.text(2018, y_merged + 7.25*lineH, 'August 2018 Trump\nAdmin. Proposed\nRollback, fix 2020\nstandards through 2026', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'bottom')
-		plt.text(2018, y_merged + 9.25*lineH, 'August 2018: Trump\nAdmin. Proposed\nRollback', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'bottom')
+		plt.text(2018, y_merged + 18.25*lineH, 'August 2018: Trump\nAdmin. Proposed\nRollback', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'bottom')
 
 
-		plt.plot((2009, 2009), (-7, 12), '-k')
+		plt.plot((2009, 2009), (-7, 9), '-k')
 		#plt.text(2008.5, 9.25+tickH, '2009 Obama Admin. National Program for\nharmonized standards between NHTSA, USEPA,\nCARB, 13 automakers', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'bottom')#, rotation = 90)
-		plt.text(2008.5, 12.25, '2009: Obama Admin. National Program', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'bottom')#, rotation = 90)
+		plt.text(2008.5, 9.25, '2009: Obama Admin. National Program', fontsize = fs, horizontalalignment = 'left', verticalalignment = 'bottom')#, rotation = 90)
 	addMergedDates()
 
 	def addMPGBars():
@@ -177,31 +180,30 @@ def makeAFigure():
 
 		for n in range(len(carAll.Year)):
 			if n<len(carAll.Year)-1:
-				addMPGdata(carAll.Year.iloc[n], carAll.CarStd.iloc[n]/5, carAll.CarStd.iloc[n+1]/5, y_merged+3*lineH/2, 1, 'car', 0)
+				addMPGdata(carAll.Year.iloc[n], carAll.CarStd.iloc[n]/5, carAll.CarStd.iloc[n+1]/5, y_merged-lineH/2, 1, 'car', 0)
 			else:
-				addMPGdata(carAll.Year.iloc[n], carAll.CarStd.iloc[n]/5, 0, y_merged+3*lineH/2, 1, 'car', 1)
+				addMPGdata(carAll.Year.iloc[n], carAll.CarStd.iloc[n]/5, 0, y_merged-lineH/2, 1, 'car', 1)
 			#ax1.add_patch(r)
 
 		for n in range(len(carObama.Year)):
 			if n<len(carObama.Year)-1:
-				if carObama.Year[n]<2020:
-					addMPGdata(carObama.Year.iloc[n], carObama.CarStd.iloc[n]/5, carObama.CarStd.iloc[n+1]/5, y_merged+3*lineH/2, 1, 'car', 0)
+				if carObama.Year[n]<2020: 
+					addMPGdata(carObama.Year.iloc[n], carObama.CarStd.iloc[n]/5, carObama.CarStd.iloc[n+1]/5, y_merged-lineH/2, 1, 'car', 0)
 				else: 
-					addMPGdata(carObama.Year.iloc[n], carObama.CarStd.iloc[n]/5, carObama.CarStd.iloc[n+1]/5, y_merged+3*lineH/2, 0.3, 'car', 0)
+					addMPGdata(carObama.Year.iloc[n], carObama.CarStd.iloc[n]/5, carObama.CarStd.iloc[n+1]/5, y_merged-lineH/2, 0.3, 'car', 0)
 			else:
-				addMPGdata(carObama.Year.iloc[n], carObama.CarStd.iloc[n]/5, 0, y_merged+3*lineH/2, 0.3, 'car', 1)
-			#ax1.add_patch(r)
-		plt.text(carObama.Year.iloc[-3]+0.25, carObama.CarStd.iloc[-3]/5+y_merged+lineH, 'Obama Rule', fontsize = fs-2, color = carcolor)
+				addMPGdata(carObama.Year.iloc[n], carObama.CarStd.iloc[n]/5, 0, y_merged-lineH/2, 0.3, 'car', 1)
+		plt.text(carObama.Year.iloc[-3]+0.25, carObama.CarStd.iloc[-3]/5+y_merged+2*lineH, 'Obama Rule', fontsize = fs-2, color = carcolor)
 
 		for n in range(len(carTrump.Year)):
 			#if n<len(carTrump.Year)-1:
 			#addMPGdata(carTrump.Year.iloc[n], carTrump.CarStd.iloc[n]/5, carTrump.CarStd.iloc[n+1]/5, y_merged+3*lineH/2, 0.4, 'car', 1)
 		#	else:
-			addMPGdata(carTrump.Year.iloc[n], carTrump.CarStd.iloc[n]/5, 0, y_merged+3*lineH/2, 0.4, 'car', 1)
+			addMPGdata(carTrump.Year.iloc[n], carTrump.CarStd.iloc[n]/5, 0, y_merged-lineH/2, 0.4, 'car', 1)
 			#ax1.add_patch(r)
-		plt.text(carTrump.Year.iloc[-3]+0.25, carTrump.CarStd.iloc[-3]/5+y_merged+lineH, 'Trump Proposal', fontsize = fs-2, color = carcolor)
+		plt.text(carTrump.Year.iloc[-3]+0.25, carTrump.CarStd.iloc[-3]/5+y_merged, 'Trump Proposal', fontsize = fs-2, color = carcolor)
 
-		plt.plot((carObama.Year.iloc[0], carObama.Year.iloc[0]), (carObama.CarStd.iloc[0]/5+y_merged+3*lineH/2, carAll.CarStd.iloc[-1]/5+y_merged+3*lineH/2), color = carcolor, alpha = 1)
+		plt.plot((carObama.Year.iloc[0], carObama.Year.iloc[0]), (carObama.CarStd.iloc[0]/5+y_merged-lineH/2, carAll.CarStd.iloc[-1]/5+y_merged-lineH/2), color = carcolor, alpha = 1)
 
 		for n in range(len(truckNHTSA.Year)):
 			if n<len(truckNHTSA.Year)-1:
@@ -212,27 +214,27 @@ def makeAFigure():
 
 		for n in range(len(truckAll.Year)):
 			if n <len(truckAll.Year)-1:
-				addMPGdata(truckAll.Year.iloc[n], truckAll.LTStd.iloc[n]/5, truckAll.LTStd.iloc[n+1]/5, y_merged+3*lineH/2, 1, 'truck', 0)
+				addMPGdata(truckAll.Year.iloc[n], truckAll.LTStd.iloc[n]/5, truckAll.LTStd.iloc[n+1]/5, y_merged-lineH/2, 1, 'truck', 0)
 			else:
-				addMPGdata(truckAll.Year.iloc[n], truckAll.LTStd.iloc[n]/5, 1, y_merged+3*lineH/2, 1, 'truck', 1)
+				addMPGdata(truckAll.Year.iloc[n], truckAll.LTStd.iloc[n]/5, 1, y_merged-lineH/2, 1, 'truck', 1)
 			#ax1.add_patch(r)
 
 		for n in range(len(truckObama.Year)):
 			if n <len(truckObama.Year)-1:
-				if truckObama.Year[n]<2020:
-					addMPGdata(truckObama.Year.iloc[n], truckObama.LTStd.iloc[n]/5, truckObama.LTStd.iloc[n+1]/5, y_merged+3*lineH/2, 1, 'truck',0)
+				if truckObama.Year[n] < 2020:
+					addMPGdata(truckObama.Year.iloc[n], truckObama.LTStd.iloc[n]/5, truckObama.LTStd.iloc[n+1]/5, y_merged-lineH/2, 1, 'truck',0)
 				else: 
-					addMPGdata(truckObama.Year.iloc[n], truckObama.LTStd.iloc[n]/5, truckObama.LTStd.iloc[n+1]/5, y_merged+3*lineH/2, 0.3, 'truck',0)
+					addMPGdata(truckObama.Year.iloc[n], truckObama.LTStd.iloc[n]/5, truckObama.LTStd.iloc[n+1]/5, y_merged-lineH/2, 0.3, 'truck',0)
 			else:
-				addMPGdata(truckObama.Year.iloc[n], truckObama.LTStd.iloc[n]/5, 0, y_merged+3*lineH/2, 0.3, 'truck',1)
+				addMPGdata(truckObama.Year.iloc[n], truckObama.LTStd.iloc[n]/5, 0, y_merged-lineH/2, 0.3, 'truck',1)
 			#ax1.add_patch(r)
 		plt.text(truckObama.Year.iloc[-3]+0.25, truckObama.LTStd.iloc[-3]/5+y_merged+lineH, 'Obama Rule', fontsize = fs-2, color = truckcolor)
 		plt.plot((truckObama.Year.iloc[0], truckObama.Year.iloc[0]), (truckObama.LTStd.iloc[0]/5+y_merged+3*lineH/2, truckAll.LTStd.iloc[-1]/5+y_merged+3*lineH/2), color = truckcolor, alpha = 0.3)
 
 
 		for n in range(len(truckTrump.Year)):
-			addMPGdata(truckTrump.Year.iloc[n], truckTrump.LTStd.iloc[n]/5, 0, y_merged+3*lineH/2, 0.4, 'truck',1)
-		plt.text(truckTrump.Year.iloc[-3]+0.25, truckTrump.LTStd.iloc[-3]/5+y_merged+lineH, 'Trump Proposal', fontsize = fs-2, color = truckcolor)
+			addMPGdata(truckTrump.Year.iloc[n], truckTrump.LTStd.iloc[n]/5, 0, y_merged-lineH/2, 0.4, 'truck',1)
+		plt.text(truckTrump.Year.iloc[-3]+0.25, truckTrump.LTStd.iloc[-3]/5+y_merged-2*lineH, 'Trump Proposal', fontsize = fs-2, color = truckcolor)
 			#ax1.add_patch(r)
 	addMPGBars()
 	
@@ -243,13 +245,16 @@ def makeAFigure():
 	ax1.add_patch(prenhtsa_line)
 	ax1.add_patch(nhtsa_line)
 	ax1.add_patch(mergednhtsa_line)
-	ax1.add_patch(nhtsaW1)
-	ax1.add_patch(nhtsaW2)
-	ax1.add_patch(nhtsa_connector)
+	#ax1.add_patch(nhtsaW1)
+	#ax1.add_patch(nhtsaW2)
+	#ax1.add_patch(nhtsa_connector)
 
 	ax1.add_patch(prepa_line)
 	#ax1.add_patch(epa_line)
 	ax1.add_patch(mergedepa_line)
+	ax1.add_patch(epaW1)
+	ax1.add_patch(epaW2)
+	ax1.add_patch(epa_connector)
 
 	ax1.add_patch(preca_line)
 	#ax1.add_patch(ca_line)
@@ -266,7 +271,7 @@ def makeAFigure():
 
 	#matplotlib.patches.Patch(edgecolor=None, facecolor=None, color=None, linewidth=None, linestyle=None, antialiased=None, hatch=None, fill=True, capstyle=None, joinstyle=None, **kwargs)
 
-	plt.savefig('TimelineFigure.png', dpi = 300)
+	plt.savefig('TimelineFigureNHTSACenter.png', dpi = 300)
 	plt.clf()
 
 makeAFigure()
